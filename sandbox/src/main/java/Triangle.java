f4da2121985b4b71e401cf19adf8dc7f3c2474ed
Александр <aassttaapp@gmail.com>
2023-11-07 10:15:07 +0300

    Добавлен метод расчета периметра треугольника

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,3 +1,20 @@
 public class Triangle {
+    public static void main(String[] args) {
+        PrintperimeterTriangle(2,4,7);
+        PrintperimeterTriangle(3,4,7);
+        PrintperimeterTriangle(2,5,7);
+        ;
+    }
 
+
+
+    ;
+
+    static void PrintperimeterTriangle(double side1, double side2, double side3) {
+        System.out.println("Периметр треугольника со сторонами " + side1 + " , " + side2 + " , " + side3 + " = " + perimetrTriangle(side1, side2, side3));
+    }
+
+    private static double perimetrTriangle(double a, double b, double c) {
+        return a + b + c;
+    };
 }
